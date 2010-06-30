2e0546d481fdb7cb8d1d244c2d56684c5b869215
Mike Dewar <md2954@columbia.edu>
2010-06-30 15:46:05 -0400

    added a file storing the class labels (picked by hand, goddamit)

--- mismatch.py
+++ mismatch.py
@@ -15,41 +15,17 @@ def gen_features(x,m,beta):
     beta : array
         all possibly kmers
     """
-    print "forming kmers"
     k = len(beta[0])
     all_kmers_in_x = [x[i:i+k] for i in range(len(x)-2)]
     alpha = np.array([list(a) for a in all_kmers_in_x])
     
     # form features
-    print "forming features"
-    
     n1 = alpha.shape[0]
     n2 = beta.shape[0]
-    
     mismatches = np.repeat(alpha, n2, 0) != np.tile(beta, (n1,1))
     tested = np.sum(mismatches,1) <= m
     tested_reshaped = np.reshape(tested,(n2,n1))
     return np.sum(tested_reshaped,1)
 
-if __name__ == "__main__":
-    
-    x = """MEHTPLLSSFPITLLDHCGGNRKIHWTRCYEQKYWLPFSCCKVASRLPCVNVRRSYMWYSKKQSKWIYLS
-ESDFRACKAGIYKRREEQEKEKLWSELCDICSWECFEYYKFRDQRLLLLLRKKIADKAQCRCRTNCKLVT
-IKHGYVRRVKTIEPCEAIELTNAETFGSNLDFAQPEMDRPEGSEERTVQTSNVVLGETNIESQDIASKEY
-SPTWDRLASSEVSDEYPMLTDRWLFWKSVKWEVNDSAFGKMLVQEKFPQSWVQMDVNVNNIPRYTNIPNF"""
-
-    beta = form_all_kmers(np.unique(x),3)
-    gen_features(x,m=2,beta=beta)
-
-    to_run = """
-    import mismatch
-    import numpy as np
-    x = 'MEHTPLLSSFPITLLDHCGGNRKIHWTRCYEQKYWLPFSCCKVASRLPCVNVRRSYMWYSKKQSKWIYLS'
-    beta = mismatch.form_all_kmers(np.unique(x),3)
-    mismatch.gen_features(x,m=2,beta=beta)
-    """
-    
-# print timeit.Timer(to_run).timeit(number=10)/10
-    
     
     
--- picorna.py
+++ picorna.py
@@ -5,34 +5,34 @@ from mismatch import *
 
 class Protein():
     def __init__(self,name):
-        print "initialised %s"%name
+        print "\tinitialised %s"%name
         self.name = name
         self.lines = []
     
     def add_line(self,line):
         self.lines.append(line)
         
-    def finish(self,k,m):
+    def finish(self,m,beta):
+        print "\tfinishing %s"%self.name
         self.data = "".join(self.lines)
-        print self.data
-        self.feature = gen_features(self.data,k,m)
+        self.feature = gen_features(self.data,m,beta)
         
     def __str__(self):
         return self.name + "\n" + self.data
 
 class Virus():
-    def __init__(self,name,k,m):
+    def __init__(self,name,m,beta):
         print "initialised %s"%name
         self.name = name
         self.proteins = []
         self.label = None
-        self.k = k
         self.m = m
+        self.beta = beta
         
     def add_line(self,line):
         if ">" in line:
             if len(self.proteins):
-                self.proteins[-1].finish(self.k,self.m)
+                self.proteins[-1].finish(self.m,self.beta)
             self.proteins.append(Protein(line))
         else:
             self.proteins[-1].add_line(line)
@@ -47,22 +47,41 @@ class Virus():
         return self.name
 
 class Picorna():
-    def __init__(self,k,m,filename='picornavirus-proteins.fasta'):
-        self.viruses = []
+    def __init__(self,k,m):
+        """
+        k : int
+            length of kmers to consider
+        m : int
+            largest number of mismatches
+        """
         self.k = k
         self.m = m
+        self.A = list('ACDEFGHIKLMNPQRSTVWY')
+        self.beta = form_all_kmers(self.A,self.k)
+        self.viruses = []
+    
+    def parse(self,filename='picornavirus-proteins.fasta'):
+        """
+        filename : string
+            filename of a multi-virus, multi-protein fasta file
+        """
         f = open(filename,'r').readlines()
         f = [fi.strip() for fi in f]
         for line in f:
-            print line
             if "NC_" in line:
                 full_name = line.split(",")[0]
                 name_elements = full_name.split(' ')
                 virus_name = ' '.join(name_elements[1:])
-                self.viruses.append(Virus(virus_name,self.k,self.m))
+                self.finish_last_protein()
+                self.viruses.append(Virus(virus_name,self.m,self.beta))
             else:
                 self.viruses[-1].add_line(line)
-        self.viruses[-1].proteins[-1].finish()
+        self.finish_last_protein()
+    
+    def finish_last_protein(self):
+        if len(self.viruses):
+            if len(self.viruses[-1].proteins):
+                self.viruses[-1].proteins[-1].finish(self.m,self.beta)
     
     def __len__(self):
         return len(self.viruses)
@@ -70,10 +89,8 @@ class Picorna():
     def __getitem__(self,i):
         return self.viruses[i]
     
-            
-        
     
 
 if __name__=="__main__":
     v = Picorna(k=3,m=2)
-    print v.viruses[6].genomes[1]
+    v.parse()
