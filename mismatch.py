6f02e22f55a3f68f02d70a155cfc6bbcb51c1a81
Mike Dewar <md2954@columbia.edu>
2010-07-21 20:43:52 -0400

    Revert "docstrings"
    This reverts commit 5a63b7f839ccf9a48cc4725ec8c347b8a301cc56.

--- mismatch.py
+++ mismatch.py
@@ -8,35 +8,18 @@ def form_all_kmers(A,k):
         alphabet - all possible characters
     k : int
         the length of subsequences you're after
-    
-    Returns
-    -------
-    beta : array
-        all possible kmers that can be formed by the alphabet A
     """
     all_kmers = itertools.product(A,repeat=k)
     return np.array([beta for beta in all_kmers])
 
 def gen_features(x,m,beta):
     """
-    a feature of `x` is the count in `x` of each kmer in `beta`, where the 
-    kmers in `x` are allowed to mismatch each element of beta by `m` 
-    mismatches.
-    
-    Arguments
-    ---------
-    
     x : list
         protein sequence
     m : int
         number of mismatches
     beta : array
         all possible kmers
-            
-    Returns
-    -------
-    features : array
-        count in `x` of each kmer in `beta` varying by `m` mismatches.
     """
     k = len(beta[0])
     all_kmers_in_x = [x[i:i+k] for i in range(len(x)) if len(x[i:i+k])==k]
