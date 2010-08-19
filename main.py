72c17a40375ee6921fb503b5e0c29a1b4772a46a
Anil Raj <ar2384@columbia.edu>
2010-08-19 18:41:48 -0400

    Boost code implemented in weave; super-fast! Estimated time for the t^th boosting round is 60*(t-1)+40. Needs some clean-up + lots of documentation. There was an issue where column slicing of numpy arrays made it Fortran contiguous, completely f$%@ing up weave; issue resolved temporarily with some transpose hacks.

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import numpy as np
-data = np.load('picorna_virii_data.npz')
-Xt = data["X"]
-Yt = data["Y"]
+import splitdata
+import boost
+import pdb
+
+data = np.load('/proj/ar2384/picorna/picorna_virii_data.npz')
+#data = np.load('MHtestdata.npz')
+Xt = data['X']
+Yt = data['Y']
+
 # make Xt, Yt memory-efficient
 # would be good to do this from the get go
-Xt = Xt.astype('int16')
-Yt = Yt.astype('int16')
+Xt = Xt.astype('float32')
+Yt = Yt.astype('float32')
 
 # number of folds of cross validation
 Nfold = 10
+
 # split the data indices into 10 random disjoint sets
 Fidx = splitdata.cv_multiclass_fold(Yt,Nfold)
 
-for f in range(Nfold):
+for f in range(3):
     # using each set as the test set and the rest as train sets
     # split the data and run boosting
     X, Y, x, y, Idx = splitdata.cv_split(Xt,Yt,Fidx[f])
-    #boost.adaboostMH(X, Y, x, y, f, model='tree')
+    boost.adaboostMH(X, Y, x, y, f, model='tree')
