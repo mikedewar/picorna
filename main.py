fce1e75430c5d5f256fd427e9061f4b3c5160cd0
Anil Raj <ar2384@columbia.edu>
2010-09-11 23:47:43 -0400

    a script to plot AUC as a function of boosting round, for different values of (k,m)

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import numpy as np
+import cPickle
 import splitdata
 import boost
 import pdb
 
-data = np.load('/proj/ar2384/picorna/picorna_virii_data.npz')
-#data = np.load('MHtestdata.npz')
-Xt = data['X']
-Yt = data['Y']
+k = 10
+m = 8
+filename = '/proj/ar2384/picorna/picorna_virii_data_%d_%d.pkl' % (k,m)
+f = open(filename,'r')
+Xt = cPickle.load(f)
+Yt = cPickle.load(f)
+kmer_dict = cPickle.load(f)
+f.close()
 
 # make Xt, Yt memory-efficient
 # would be good to do this from the get go
-Xt = Xt.astype('float32')
-Yt = Yt.astype('float32')
+Xt = Xt.astype('int16')
+Yt = Yt.astype('int16')
 
 # number of folds of cross validation
 Nfold = 10
@@ -20,7 +25,8 @@ Nfold = 10
 Fidx = splitdata.cv_multiclass_fold(Yt,Nfold)
 
 for f in range(3):
+    params = (f,k,m)
     # using each set as the test set and the rest as train sets
     # split the data and run boosting
     X, Y, x, y, Idx = splitdata.cv_split(Xt,Yt,Fidx[f])
-    boost.adaboostMH(X, Y, x, y, f, model='tree')
+    boost.adaboostMH(X, Y, x, y, params, kmer_dict, model='tree')
