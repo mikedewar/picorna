6732e58c813cfc8edf5c03513619ef9162590737
Mike Dewar <md2954@columbia.edu>
2010-09-16 15:47:58 -0400

    now saves the labeled feature set

--- nb.py
+++ nb.py
@@ -1,8 +1,8 @@
 from nltk.classify import naivebayes as nb
+import numpy as np
 import cPickle
 
-
-fh = open('/Users/mike/Data/picorna_virii_data_8_2.pkl')
+fh = open('picorna_virii_data_8_2.pkl')
 X = cPickle.load(fh)
 Y = cPickle.load(fh)
 z = cPickle.load(fh)
@@ -10,10 +10,9 @@ z = cPickle.load(fh)
 v = z.values()
 labels = np.argmax(Y,axis=0)
 
-labeled_featuresets = [
-    (dict(zip(v,data)),y) 
-    for (data,y) in zip(X.T,labels)
-]
+labeled_featuresets = [(dict(zip(v,data)),y) for (data,y) in zip(X.T,labels)]
 
-model = nb.NaiveBayesClassifier.train(labeled_featuresets)
+cPickle.dump(labeled_featuresets,file=open('labeled_featuresets.pkl'))
+
+#model = nb.NaiveBayesClassifier.train(labeled_featuresets)
 
