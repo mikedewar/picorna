258a3d555e7e132a66ba124b271180e1a872bdf5
Anil Raj <ar2384@columbia.edu>
2010-09-21 17:12:05 -0400

    made some change, hopefully about to overwrite

--- nb.py
+++ nb.py
@@ -2,7 +2,7 @@ from nltk.classify import naivebayes as nb
 import numpy as np
 import cPickle
 
-fh = open('picorna_virii_data_8_2.pkl')
+fh = open('/proj/ar2384/picorna/picorna_virii_data_8_2.pkl')
 X = cPickle.load(fh)
 Y = cPickle.load(fh)
 z = cPickle.load(fh)
@@ -12,7 +12,16 @@ labels = np.argmax(Y,axis=0)
 
 labeled_featuresets = [(dict(zip(v,data)),y) for (data,y) in zip(X.T,labels)]
 
-cPickle.dump(labeled_featuresets,file=open('labeled_featuresets.pkl'))
+cPickle.dump(
+    labeled_featuresets,
+    file=open('/proj/ar2384/picorna/labeled_featuresets.pkl','w')
+)
 
-#model = nb.NaiveBayesClassifier.train(labeled_featuresets)
+model = nb.NaiveBayesClassifier.train(labeled_featuresets)
+
+cPickle.dump(
+    model,
+    file=open('/proj/ar2384/picorna/nbmodel.pkl','w')
+)
+
 
