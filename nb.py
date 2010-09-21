598f10fb4ba8992dc0215179e8052e79b4e22af9
Mike Dewar <md2954@columbia.edu>
2010-09-21 16:35:44 -0400

    updated naive bayes code to use cross validaion

--- nb.py
+++ nb.py
@@ -1,18 +1,35 @@
 from nltk.classify import naivebayes as nb
 import numpy as np
 import cPickle
+import splitdata
 
 fh = open('picorna_virii_data_8_2.pkl')
 X = cPickle.load(fh)
 Y = cPickle.load(fh)
 z = cPickle.load(fh)
-
 v = z.values()
+split_indices = splitdata.cv_multiclass_fold(Y,10)
+
 labels = np.argmax(Y,axis=0)
+labelled_featuresets = [(dict(zip(v,data)),y) for (data,y) in zip(X.T,labels)]
 
-labeled_featuresets = [(dict(zip(v,data)),y) for (data,y) in zip(X.T,labels)]
-
-cPickle.dump(labeled_featuresets,file=open('labeled_featuresets.pkl'))
-
-#model = nb.NaiveBayesClassifier.train(labeled_featuresets)
-
+test_labels=[]
+true_labels=[]
+for i,train_indices in enumerate(split_indices):
+    test_indices = set(range(Y.shape[1])).difference(train_indices)
+    # train
+    train_features = [labelled_featuresets[i] for i in train_indices]
+    model = nb.NaiveBayesClassifier.train(train_featuresets)
+    # test
+    test_features = [labelled_featuresets[i] for i in test_indices]
+    label = [model.classify(featureset) for featureset in test_features]
+    # collect
+    true_labels.append(Y[:,test_indices])
+    test_labels.append(label)
+# save
+fh = open('/proj/ar2384/picorna/labels.pkl','w')
+labels = {
+    "true":true_labels,
+    "test":test_labels
+}
+cPickle.Pickler(fh,protocol=2).dump(labels)
