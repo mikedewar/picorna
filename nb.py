08773e49ebbbc384e0de715840f0337f1e3ee660
Mike Dewar <md2954@columbia.edu>
2010-09-23 15:01:55 -0400

    merging..

--- nb.py
+++ nb.py
@@ -7,6 +7,10 @@ fh = open('picorna_virii_data_8_2.pkl')
 X = cPickle.load(fh)
 Y = cPickle.load(fh)
 z = cPickle.load(fh)
+
+# just pull out ten features from X to make sure the whole thing works
+X = X[:10,:]
+
 v = z.values()
 split_indices = splitdata.cv_multiclass_fold(Y,10)
 
@@ -16,13 +20,13 @@ labelled_featuresets = [(dict(zip(v,data)),y) for (data,y) in zip(X.T,labels)]
 test_labels=[]
 true_labels=[]
 for i,train_indices in enumerate(split_indices):
-    test_indices = set(range(Y.shape[1])).difference(train_indices)
+    test_indices = list(set(range(Y.shape[1])).difference(train_indices))
     # train
     train_features = [labelled_featuresets[i] for i in train_indices]
-    model = nb.NaiveBayesClassifier.train(train_featuresets)
+    model = nb.NaiveBayesClassifier.train(train_features)
     # test
     test_features = [labelled_featuresets[i] for i in test_indices]
-    label = [model.classify(featureset) for featureset in test_features]
+    label = [model.classify(featureset[0]) for featureset in test_features]
     # collect
     true_labels.append(Y[:,test_indices])
     test_labels.append(label)
